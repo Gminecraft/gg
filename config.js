b5afce0e07d2c24684dff0968d6f792f454a6d41
Gminecraft <113657224+Gminecraft@users.noreply.github.com>
2025-01-25 21:01:50 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+export default {
+    scales: ['0.3', '0.5', '0.8', '1', '1.2', '1.5', '2', '2.5', '3', '5', '7', '10'],
+    actions: {
+        'Bob': 'animation.humanoid.bob',
+        'Flip': 'animation.humanoid.flip',
+        'Champion': 'animation.humanoid.champion',
+        'Stomp': 'animation.humanoid.stomp',
+        'Hi': 'animation.humanoid.hi',
+        'Moruto': 'animation.humanoid.naruto',
+        'Command': 'animation.humanoid.command',
+        'Sentado': 'animation.humanoid.sentado',
+        'Sentado 2': 'animation.humanoid.sentado2',
+        'Deitado': 'animation.humanoid.deitado',
+        'Float': 'animation.humanoid.float',
+        'Salute': 'animation.humanoid.salute',
+        // 'Watch': null
+    }
+};
